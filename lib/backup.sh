3c5709f96d42f40c5467eb82208a12d60efce13b
Denys Dovhan <denysdovhan@gmail.com>
2021-03-13 18:57:27 +0200

    Implement backup restoration

--- lib/backup.sh
+++ lib/backup.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Check if project exists
-if [[ -z $PROJECT_DOCKER_COMPOSE ]]; then
-  echo "There is not docker-compose.yaml file in current project!"
-  echo "Please, intialize the project first."
-  exit 1
-fi
-
 BACKUP_COMMAND="$2"
+BACKUP_FOLDER="$PROJECT_DIR/${BACKUPS:-.backups}";
 
 if [[ -z "$BACKUP_COMMAND" ]]; then
   BACKUP_COMMAND=$(
@@ -21,13 +15,15 @@ fi
 
 case "$BACKUP_COMMAND" in
   "create" | "new" | "now")
-    TIMESTAMP=$(date +"%Y-%m-%d_%H:%M:%S");
-    BACKUP_FOLDER="$PROJECT_DIR/$BACKUPS";
-    BACKUP_FILE="$BACKUP_FOLDER/backup-$TIMESTAMP.tag.gz";
+    TIMESTAMP=$(date +"%Y-%m-%d_%H-%M%S");
+    BACKUP_FILE="$BACKUP_FOLDER/backup_$TIMESTAMP.tar.gz";
     mkdir -p "$BACKUP_FOLDER"
 
     echo "Compressing $PROJECT_DIR into $BACKUP_FILE..."
-    tar --exclude="$BACKUPS" -czvf "$BACKUP_FILE" "$PROJECT_DIR"
+
+    pushd "$PROJECT_DIR" > /dev/null || exit
+    tar --exclude="$BACKUPS" -czvf "$BACKUP_FILE" .
+    popd > /dev/null || exit
 
     if [[ ! -f "$BACKUP_FILE" ]]; then
       echo "Something went wrong!"
@@ -38,26 +34,70 @@ case "$BACKUP_COMMAND" in
     echo "Compressed! File size: $(du -h $BACKUP_FILE)"
 
     echo "Removing old backups..."
-    find $BACKUP_FOLDER -mtime +10 -type f -delete;
+    find "$BACKUP_FOLDER" -mtime +10 -type f -delete;
   ;;
   "restore")
-    BACKUP_FILE="$3"
+    # Check for argument
+    if [[ -n "$3" ]]; then
+      BACKUP_FILE="$3"
+    fi
+
+    # If argument is not present, then ask user
+    if [[ -z "$3" ]]; then
+      for file in "$BACKUP_FOLDER"/*.tar.gz; do
+        if [ ! -f $file ]; then
+          whiptail \
+            --title "Home Stack" \
+            --msgbox "No backups found. Try creating one." \
+            8 78
+          exit
+        fi
+        filename=$(basename $file)
+        filesize=$(du -sh $file | cut -f1)
+        created=$(date +%^b\ %-e\ %Y -r $file)
+
+        LIST_ENTRIES+=("$filename")
+        LIST_ENTRIES+=("    -    $created  [$filesize]")
+      done
+
+      SELECTED_BACKUP=$(whiptail \
+        --title "Home Stack" \
+        --menu "Choose a backup:" \
+        20 78 12 \
+        "${LIST_ENTRIES[@]}" \
+        3>&1 1>&2 2>&3)
+
+      if [[ $? != 0 ]]; then
+        echo "Initialising new project is aborted by the user"
+        exit
+      fi
+
+      BACKUP_FILE="$BACKUP_FOLDER/$SELECTED_BACKUP"
+    fi
 
     if [[ ! -f "$BACKUP_FILE" ]]; then
       echo "File $BACKUP_FILE doesn't exist!"
       exit 1;
     fi
 
-    echo "Purging all files except $BACKUPS"
-    find $PROJECT_DIR -not -path "*/$BACKUPS*" -type f -delete
+    if (whiptail \
+      --title "Restoring a backup" \
+      --yesno "Everything will be erased. The content of this backup will be restored. Are you sure?" \
+      20 78);
+    then
+      echo "Restoring $BACKUP_FILE..."
 
-    tar -zxvf $BACKUP_FILE
+      echo "Purging all files except for $BACKUP_FOLDER..."
+      pushd "$PROJECT_DIR" > /dev/null || exit
+      find . -not -path "$BACKUP_FOLDER/*" -delete
 
-    echo "$BACKUP_FILE restored!"
+      tar -zxvf "$BACKUP_FILE" "$PROJECT_DIR"
+      popd > /dev/null || exit
+    fi
   ;;
   *)
     # TODO: Print help here
-    echo "Unknown server command"
+    echo "Unknown backup command"
     exit 1
   ;;
 esac
